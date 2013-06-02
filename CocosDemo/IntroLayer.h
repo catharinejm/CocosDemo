4d396fcd9cb15a796c8bbaee6cd9cebfdaac1815
Jon Distad <jon.distad@gmail.com>
2013-06-02 10:51:23 -0400

    Initial Commit

---
+++ CocosDemo/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  CocosDemo
+//
+//  Created by Jon Distad on 6/2/13.
+//  Copyright Jon Distad 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
