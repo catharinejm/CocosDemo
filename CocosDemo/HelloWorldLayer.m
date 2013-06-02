296b65be3554e0f25d50ae3f0d2db70bc728b0b8
Jon Distad <jon.distad@gmail.com>
2013-06-02 18:44:49 -0400

    Resolve deprecation warning.

--- CocosDemo/HelloWorldLayer.m
+++ CocosDemo/HelloWorldLayer.m
@@ -223,12 +223,12 @@
 -(void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController
 {
 	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-	[[app navController] dismissModalViewControllerAnimated:YES];
+	[[app navController] dismissViewControllerAnimated:YES completion:nil];
 }
 
 -(void) leaderboardViewControllerDidFinish:(GKLeaderboardViewController *)viewController
 {
 	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
-	[[app navController] dismissModalViewControllerAnimated:YES];
+	[[app navController] dismissViewControllerAnimated:YES completion:nil];
 }
 @end
